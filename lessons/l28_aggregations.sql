65998b4fd9dc2a8fb10fb020157df34627757f66
ssi112 <brrg58@yahoo.com>
2019-12-27 20:27:30 -0500

    lesson 28 aggregations

--- lessons/l28_aggregations.sql
+++ lessons/l28_aggregations.sql
@@ -177,3 +177,275 @@ JOIN region r
 GROUP BY r.name, w.channel
 ORDER BY channel_occurrences DESC;
 
+
+/*
+1) Use DISTINCT to test if there are any accounts associated with more than one region.
+*/
+SELECT DISTINCT a.name AS "Account Name", r.name AS "Region Name"
+FROM accounts a
+JOIN sales_reps s
+  ON a.sales_rep_id = s.id
+JOIN region r
+  ON s.region_id = r.id
+ORDER BY a.name;
+
+/*
+2) Have any sales reps worked on more than one account?
+*/
+SELECT DISTINCT a.name AS "Account Name", s.name AS "Sales Rep Name"
+FROM accounts a
+JOIN sales_reps s
+  ON a.sales_rep_id = s.id
+ORDER BY s.name;
+
+
+/* HAVE YOU OR HAVE YOU NOT */
+SELECT o.account_id, SUM(o.total_amt_usd) AS sum_tot_usd
+FROM orders o
+GROUP BY 1
+--HAVING sum_tot_usd >= 250000 -- PostgreSQL = NO GO!
+HAVING SUM(o.total_amt_usd) >= 250000
+ORDER BY 2 DESC;
+
+/*
+1) How many of the sales reps have more than 5 accounts that they manage?
+*/
+SELECT s.id, s.name as sales_rep_name, COUNT(*) as num_accounts
+FROM sales_reps s
+JOIN accounts a
+  ON s.id = a.sales_rep_id
+GROUP BY 1, 2
+HAVING COUNT(*) > 5
+ORDER BY num_accounts;
+
+/*
+3) which account has the most orders
+*/
+SELECT DISTINCT account_id, COUNT(total) as num_orders
+FROM orders o
+GROUP BY 1
+--HAVING MAX(total) don't need HAVING for this
+ORDER BY 2 DESC
+LIMIT 1 ;
+
+/*
+4) How many accounts spent more than 30,000 usd total across all orders?
+*/
+SELECT a.name, COUNT(*), SUM(total_amt_usd) AS tot_amt_usd
+FROM orders o
+JOIN accounts a
+  ON o.account_id = a.id
+GROUP BY a.name
+HAVING SUM(total_amt_usd) > 30000
+ORDER BY a.name;
+
+/*
+8) Which accounts used facebook as a channel to contact customers more than 6 times?
+*/
+SELECT a.name as acct_name, w.channel as fuckbook, COUNT(*) as abused
+FROM accounts a
+JOIN web_events w
+  ON a.id = w.account_id
+WHERE w.channel = 'facebook'
+GROUP BY a.name, w.channel
+HAVING COUNT(*) > 6
+ORDER BY abused, a.name;
+
+
+/* ----------------------------------------------------------------------------------------
+    DATES DB stored from biggest to most granular | YYYY MM DD HH MM SS 
+
+    PostgreSQL uses DATE_TRUNC('day', datecolumn) to accomplish same
+    https://stackoverflow.com/questions/28638907/postgresql-sqlite-date-trunc-equivalent
+    
+    For PostgreSQL also see DATE_PART: parms: 'second', 'day', 'month', 'year', 'dow'
+    https://www.postgresql.org/docs/9.1/functions-datetime.html
+*/
+SELECT DATETIME(o.occurred_at, 'start of day'), 
+       strftime('%m', o.occurred_at) as Month,
+       strftime('%d', o.occurred_at) as Day,
+       strftime('%Y', o.occurred_at) as Year,
+       SUM(standard_qty) AS sum_std_qty
+FROM orders o
+GROUP BY  datetime(o.occurred_at, 'start of day')
+ORDER BY  datetime(o.occurred_at, 'start of day');
+
+/*
+1) Find the sales in terms of total dollars for all orders in each year, ordered from greatest to least. 
+Do you notice any trends in the yearly sales totals?
+*/
+SELECT strftime('%Y', o.occurred_at) as Year,
+       SUM(total_amt_usd) AS sum_tot_usd
+FROM orders o
+GROUP BY Year
+ORDER BY sum_tot_usd DESC;
+
+
+/*
+2) Which month did Parch & Posey have the greatest sales in terms of total dollars? 
+Are all months evenly represented by the dataset?
+*/
+SELECT strftime('%m', o.occurred_at) as Month,
+       SUM(total_amt_usd) AS sum_tot_usd
+FROM orders o
+WHERE o.occurred_at BETWEEN '2014-01-01' AND '2017-01-01'
+GROUP BY Month
+ORDER BY sum_tot_usd DESC;
+
+/*
+3) Which year did Parch & Posey have the greatest sales in terms of total number of orders? 
+Are all years evenly represented by the dataset?
+*/
+SELECT strftime('%Y', o.occurred_at) as Year,
+       SUM(total) AS total
+FROM orders o
+GROUP BY Year
+ORDER BY total DESC
+LIMIT 1;
+
+/*
+4) Which month did Parch & Posey have the greatest sales in terms of total number of orders? 
+Are all months evenly represented by the dataset?
+*/
+SELECT strftime('%m', o.occurred_at) as Month,
+       SUM(total) AS total
+FROM orders o
+GROUP BY Month
+ORDER BY total DESC;
+
+/*
+5) In which month of which year did Walmart spend the most on gloss paper in terms of dollars?
+*/
+SELECT strftime('%Y', o.occurred_at) as Year,
+       strftime('%m', o.occurred_at) as Month,
+       SUM(gloss_amt_usd) AS gloss_amt_usd
+FROM orders o
+JOIN accounts a
+  ON o.account_id = a.id
+WHERE a.name = 'Walmart'
+GROUP BY Month, Year
+ORDER BY gloss_amt_usd DESC;
+
+/* 5) POSTGRESQL */
+SELECT DATE_PART('year', o.occurred_at) as Year,
+       DATE_PART('month', o.occurred_at) as Month,
+       SUM(gloss_amt_usd) AS gloss_amt_usd
+FROM orders o
+JOIN accounts a
+  ON o.account_id = a.id
+WHERE a.name = 'Walmart'
+GROUP BY Month, Year
+ORDER BY gloss_amt_usd DESC;
+
+
+/* ------------------------------------------------------------------------------------------------ */
+/* HEAD CASE - derived columns */
+SELECT id, account_id, occurred_at, channel,
+       CASE
+            WHEN channel = 'facebook' OR channel = 'direct'
+                 THEN 'Yea'
+                 ELSE 'Nay'
+       END AS "Something"
+FROM web_events w
+ORDER BY occurred_at;
+
+SELECT id, 
+       CASE WHEN standard_qty = 0 OR standard_qty IS NULL 
+       THEN 0
+       ELSE standard_amt_usd / standard_qty END AS unit_price
+FROM orders
+LIMIT 10;
+
+SELECT CASE WHEN total > 500  
+       THEN 'Over 500'
+       ELSE '500 or Under' END AS group_total,
+       COUNT(*) AS order_count
+FROM orders
+GROUP BY 1;
+
+/*
+1) Write a query to display for each order, the account ID, total amount of the order, and the level of the order - 
+ ‘Large’ or ’Small’ - depending on if the order is $3000 or more, or smaller than $3000.
+*/
+SELECT account_id, total_amt_usd,
+       CASE WHEN total_amt_usd > 3000  
+       THEN 'Large'
+       ELSE 'Small' END AS order_size
+FROM orders
+GROUP BY 1, 2;
+
+/*
+2) Write a query to display the number of orders in each of three categories, based on the total number of items in each order. 
+The three categories are: 'At Least 2000', 'Between 1000 and 2000' and 'Less than 1000'
+*/
+SELECT CASE 
+         WHEN total > 2000 THEN 'At Least 2000'
+         WHEN total >= 1000 AND total <= 2000 THEN 'Between 1000 and 2000'
+         ELSE 'Less than 1000'
+       END AS category,
+       COUNT(*) cat_count
+FROM orders
+GROUP BY 1;
+
+/*
+3) We would like to understand 3 different levels of customers based on the amount associated with their purchases. 
+The top level includes anyone with a Lifetime Value (total sales of all orders) greater than 200,000 usd. 
+The second level is between 200,000 and 100,000 usd. 
+The lowest level is anyone under 100,000 usd. 
+Provide a table that includes the level associated with each account. 
+You should provide the account name, the total sales of all orders for the customer, and the level.
+Order with the top spending customers listed first.
+*/
+SELECT a.name, SUM(total_amt_usd) AS total_sales,
+       CASE 
+         WHEN SUM(o.total_amt_usd) > 200000 THEN 'Tier 1'
+         WHEN SUM(o.total_amt_usd) >= 100000 AND SUM(o.total_amt_usd) <= 200000 THEN 'Tier 2'
+         ELSE 'Tier 3'
+       END AS lifetime_value
+FROM orders o
+JOIN accounts a
+  ON o.account_id = a.id
+GROUP BY 1
+ORDER BY 3, 2 DESC, 1;
+
+/*
+4) We would now like to perform a similar calculation to the first, 
+but we want to obtain the total amount spent by customers only in 2016 and 2017.
+Keep the same levels as in the previous question. Order with the top spending customers listed first.
+*/
+SELECT a.name, SUM(total_amt_usd) AS total_sales,
+       CASE 
+         WHEN SUM(o.total_amt_usd) > 200000 THEN 'Tier 1'
+         WHEN SUM(o.total_amt_usd) >= 100000 AND SUM(o.total_amt_usd) <= 200000 THEN 'Tier 2'
+         ELSE 'Tier 3'
+       END AS lifetime_value
+FROM orders o
+JOIN accounts a
+  ON o.account_id = a.id
+WHERE strftime('%Y', o.occurred_at) IN ('2016', '2017')
+-- for PostgreSQL
+--WHERE DATE_PART('year', o.occurred_at) IN (2016, 2017)
+GROUP BY 1
+ORDER BY 3, 2 DESC, 1;
+
+
+/*
+6) Identify top performing sales reps, which are sales reps associated with more than 200 orders or more than 750000 in total sales.
+The middle group has any rep with more than 150 orders or 500000 in sales. 
+Create a table with the sales rep name, the total number of orders, total sales across all orders, 
+and a column with top, middle, or low depending on this criteria. 
+Place the top sales people based on dollar amount of sales first in your final table.
+*/
+SELECT s.name, COUNT(*) order_cnt, SUM(o.total_amt_usd) total_spent, 
+      CASE 
+           WHEN COUNT(*) > 200 OR SUM(o.total_amt_usd) > 750000 THEN 'top'
+           WHEN COUNT(*) > 150 OR SUM(o.total_amt_usd) > 500000 THEN 'middle'
+           ELSE 'low' 
+      END AS sales_rep_level
+FROM orders o
+JOIN accounts a
+  ON o.account_id = a.id 
+JOIN sales_reps s
+  ON s.id = a.sales_rep_id
+GROUP BY s.name
+ORDER BY 3 DESC;
